5c9d9fa0f933910e638541b6916ef6a4233bc38e
Mikal-E <melamin101@gmail.com>
2026-06-10 11:37:23 -0400

    Created async function to load all users for team member page container. Added event handler to load users when page has loaded. Added event handler for search bar to retrieve data.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,55 @@ import { getUsers, searchUsers, getSingleUser, addNewUser } from "./api.js";
 // Import Of Functions From ui.js
 
 import { renderUsers, renderProfile, showError } from "./ui.js";
+
+// Async Function To Load All Users For The Team Member Page By Populating The Team Member Container
+
+async function loadUsers() {
+
+    try {
+
+        const data = await getUsers();
+        renderUsers(data.users);
+        
+    } catch (error) {
+
+        showError("Failed to load team members.");
+        
+    }
+    
+}
+
+// loadUsers Function Called Inside Event Handler To Run When Page Has Loaded
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    loadUsers();
+
+    }
+);
+
+// Event Handler For Search Bar (Input) In Order To Search API & Retrieve Data
+document.getElementById("search-bar").addEventListener("input", async (e) => {
+
+    const query = e.target.value.trim();
+
+    if (query === "") {
+
+        loadUsers();
+        return;
+        
+    }
+
+    try {
+
+        const data = await searchUsers(query);
+        renderUsers(data.users);
+        
+    } catch (error) {
+
+        showError("Failed to search team members.");
+        
+    }
+
+    }
+);
